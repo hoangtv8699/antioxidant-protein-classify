e16ee22d6d1d4513b965d87d25d386736edb9774
Hiddenpants <hoang.tv173129@sis.hust.edu.vn>
2020-11-25 12:58:24 +0700

    optimize code

--- train_pssm.py
+++ train_pssm.py
@@ -2,11 +2,67 @@
 
 import numpy as np
 from sklearn.model_selection import StratifiedKFold
+from tensorflow import keras
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import ZeroPadding2D
+from tensorflow.keras.models import Sequential
 
-from models import models
 from utils.helpers import *
 
-MAX_LEN = 400
+
+def models():
+    model = Sequential([
+        Input(shape=(20, 400, 1)),
+
+        ZeroPadding2D(1),
+        Conv2D(32, (3, 3), activation='relu'),
+        BatchNormalization(),
+        MaxPooling2D(2, 2),
+        Dropout(0.4),
+
+        ZeroPadding2D(1),
+        Conv2D(64, (3, 3), activation='relu'),
+        BatchNormalization(),
+        MaxPooling2D(2, 2),
+        Dropout(0.4),
+
+        ZeroPadding2D(1),
+        Conv2D(128, (3, 3), activation='relu'),
+        BatchNormalization(),
+        MaxPooling2D(2, 2),
+        Dropout(0.4),
+
+        ZeroPadding2D(1),
+        Conv2D(256, (3, 3), activation='relu'),
+        BatchNormalization(),
+        MaxPooling2D(2, 2),
+        Dropout(0.4),
+
+        Flatten(),
+        # Reshape((-1, 1)),
+        #
+        # LSTM(64),
+        # Dropout(0.2),
+
+        Dense(16, activation='relu'),
+        Dropout(0.4),
+        Dense(2, activation='softmax')
+    ])
+
+    optimizer = keras.optimizers.Adam(lr=0.0001)
+
+    model.compile(
+        optimizer=optimizer,
+        loss='sparse_categorical_crossentropy',
+        metrics=[sensitivity, specificity, 'accuracy']
+    )
+    return model
 
 
 def train(n_splits, path, batch_size, epochs, random_state):
